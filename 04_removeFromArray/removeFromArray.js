ac96dd73b32142d4ee0abe679b71bd3dede16fbf
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-20 18:20:38 +0200

    feat: Solve test lesson 4 for removeFromArray()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...valuesToRemove) {
+    // iterate through all values to remove
+    for (let j = 0; j < valuesToRemove.length; j++) {
+        // remove one value from entire array
+        for (let i = 0; i < myArray.length; i++) {
+            if (myArray[i] === valuesToRemove[j]) {
+                myArray.splice(i, 1);
+                // decrease count by 1 to check current index again
+                // because current element was deleted and shifted all
+                // other elements
+                i--;
+            }
+        }
+    }
+    return myArray;
 };
 
 // Do not edit below this line
