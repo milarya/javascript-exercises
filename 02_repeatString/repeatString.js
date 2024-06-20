9ee1865dfe053025842b5573d4dc946c9e807feb
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-20 16:24:57 +0200

    feat: Solve test automation lessons 1 + 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    // check if user input is valid
+    if (repetitions < 0) {
+        return 'ERROR';
+    }
+    // calculate the resulting string
+    let repeatedString = '';
+    for (let i = 0; i < repetitions; i++) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
