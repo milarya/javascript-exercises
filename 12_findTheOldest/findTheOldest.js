89c7c883fae24fdcf5bdf1d6114242a7d99b38b6
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-24 17:04:23 +0200

    feat: Solve lesson 12 - find oldest in array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    console.table(arr);
+    let foundPersons = arr
+        .sort((personA, personB) => {
+            let ageA = (personA.yearOfDeath || 2024 ) - personA.yearOfBirth;
+            let ageB = (personB.yearOfDeath || 2024) - personB.yearOfBirth;
+            return ageA > ageB ? -1 : 1
+        })
+    console.table(foundPersons);
+    return foundPersons[0];
 };
 
 // Do not edit below this line
