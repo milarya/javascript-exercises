4484608dbe23d3d68fafc84e13e8852dcc84dfec
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-24 16:25:37 +0200

    feat: Solve lesson 9 - palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let sanitizedString = string
+        .toLowerCase()
+        .replace(/[^a-z0-9]/g, '');
+    let reversedString = sanitizedString
+        .split('')
+        .reverse()
+        .join('');
+    if (sanitizedString == reversedString) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
