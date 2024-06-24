5d08d99efe8480a1c8ce2ddf179316c1ad08d0a0
Nina T <50660462+milarya@users.noreply.github.com>
2024-06-24 16:34:32 +0200

    feat: Solve lesson 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (+number < 0) {
+        return 'OOPS';
+    }
+    if (+number === 0) {
+        return 0;
+    }
+    let fibArray = [1, 1]
+    for (let i = 2; i < +number; i++) {
+        fibArray.push(fibArray[i-2] + fibArray[i-1]);
+    }
+    return fibArray[+number-1];
 };
 
 // Do not edit below this line
